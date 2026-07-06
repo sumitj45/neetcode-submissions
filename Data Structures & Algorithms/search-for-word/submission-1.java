9b25deffa9854b9cab97adb64729dd87f638ae8e
Sumit Jaiswal <90706283+sumitj45@users.noreply.github.com>
2026-07-06 23:00:21 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int row=board.length;
+        int col=board[0].length;
+        for(int r=0;r<row;r++){
+        for(int c=0;c<col;c++){
+            if(dfs(board,word,r,c,0))return true;
+        }}
+        return false;
+    }
+
+    private boolean dfs(char[][] board,String word,int row,int col,int index){
+
+
+        if(index==word.length()){
+            
+            return true;
+        }
+        //edge cases
+        if(row<0 || col < 0 || row>=board.length || col >=board[0].length|| board[row][col]!=word.charAt(index)){
+            return false;
+        }
+
+        char temp=board[row][col];
+        board[row][col]='#';
+
+        boolean found= dfs(board,word,row+1,col,index+1)||
+                       dfs(board,word,row-1,col,index+1)||
+                       dfs(board,word,row,col+1,index+1)||
+                       dfs(board,word,row,col-1,index+1);
+
+        board[row][col]=temp;
+
+        return found;
+
+        
+    }
+}
