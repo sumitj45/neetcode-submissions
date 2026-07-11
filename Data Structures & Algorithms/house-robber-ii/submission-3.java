75efbec26cd7f0dc6e48c177a8e7d2daa1b38f25
Sumit Jaiswal <90706283+sumitj45@users.noreply.github.com>
2026-07-11 07:07:52 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+     
+    public int rob(int[] nums) {
+        int n=nums.length;
+       int []dp=new int [n];
+       if(n==1)return nums[0];
+        //base case
+       return Math.max(
+          robRange(nums,0,n-2),
+          robRange(nums,1,n-1)
+        );
+    }
+    private int robRange(int[]nums,int start,int end){
+        int prev1=0;
+        int prev2=0;
+
+        for(int i=start;i<=end;i++){
+            int curr=Math.max(prev1,nums[i]+prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+}
